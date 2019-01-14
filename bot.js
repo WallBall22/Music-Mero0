1fdf80dcc081b7532b5f5d450a986b3882abac80
Amr <44549704+WallBall22@users.noreply.github.com>
2019-01-14 16:40:46 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const client2 = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
